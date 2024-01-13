49e26c93543c7947e87c93ba93ac27a32e01591e
omidjamal.oji <omidjamal.oji@gmail.com>
2024-01-13 12:08:07 +0330

    Show and update list of chatUsers with firebase

--- src/components/Sidebar/ChatsUser.jsx
+++ src/components/Sidebar/ChatsUser.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import { doc, onSnapshot } from "firebase/firestore";
+import {db} from "../../firebase";
+import {AuthContext} from "../../context/AuthContext";
 
 const ChatsUser = () => {
+    const [chatsUser, setChatsUSer] = useState([]);
+
+    const {currentUser} = useContext(AuthContext)
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                setChatsUSer(doc.data())
+            });
+
+            return () => {
+                unsub();
+            }
+        }
+
+        currentUser.uid && getChats();
+    }, [currentUser.uid]);
     return (
         <div className="chats">
-            <div className="userChat">
-                <img
-                    src="https://images.pexels.com/photos/19781449/pexels-photo-19781449/free-photo-of-close-up-of-an-eagle.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                    alt=""/>
-                <div className="userChatInfo">
-                    <span>Omid</span>
-                    <p>Hello</p>
+            {Object.entries(chatsUser)?.map((chat) => (
+                <div className="userChat" key={chat[0]}>
+                    <img
+                        src={chat[1].userInfo.photoURL}
+                        alt=""/>
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>{chat[1].userInfo.lastMessage?.text}</p>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
