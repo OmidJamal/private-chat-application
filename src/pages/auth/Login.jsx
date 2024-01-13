7913d0b7da5c055485c80f3024a933df54ac64c2
omidjamal.oji <omidjamal.oji@gmail.com>
2024-01-13 10:59:33 +0330

    Validation of forms using Formik

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import Add from "../../assets/images/addAvatar.png";
+import {Alert, CircularProgress} from "@mui/material";
 
 const Login = () => {
     const [err, setErr] = useState(false);
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const email = e.target[0].value;
-        const password = e.target[1].value;
+    const handleSubmit = async (values) => {
+        const email = values.email
+        const password = values.password
 
         try {
             await signInWithEmailAndPassword(auth, email, password);
@@ -22,14 +24,43 @@ const Login = () => {
     return (
         <div className="formContainer">
             <div className="formWrapper">
-                <span className="logo">Lama Chat</span>
+                <span className="logo">Omit Chat</span>
                 <span className="title">Login</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
-                    <button>Sign in</button>
-                    {err && <span>Something went wrong</span>}
-                </form>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: ""
+                    }}
+                    validate={values => {
+                        const error = {}
+                        if (!values.email) {
+                            error.email = 'email is required';
+                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                            error.email = 'email is invalid';
+                        }
+                        if (!values.password) {
+                            error.password = 'password is required';
+                        } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/i.test(values.password)) {
+                            error.password =
+                                'password must have one letter and one number and have at least 6 characters';
+                        }
+                        return error;
+                    }}
+                    onSubmit={(values) => handleSubmit(values)}
+                >
+                    {({isSubmitting}) => (
+                        <Form>
+                            <Field type="email" placeholder="email" name="email"/>
+                            <ErrorMessage className="errorMessage" name="email" component="div"/>
+                            <Field type="password" placeholder="password" name="password"/>
+                            <ErrorMessage className="errorMessage" name="password" component="div"/>
+                            <button type="submit" disabled={isSubmitting}>
+                                { isSubmitting ? <CircularProgress /> : "Sign in"}
+                            </button>
+                            { err && <Alert severity="error">Error communicating with the server</Alert>}
+                        </Form>
+                    )}
+                </Formik>
                 <p>You don't have an account? <Link to="/register">Register</Link></p>
             </div>
         </div>
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Add from "../../assets/images/addAvatar.png";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, db, storage } from "../../firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore";
+import {createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {auth, db, storage} from "../../firebase";
+import {ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
+import {doc, setDoc} from "firebase/firestore";
 import {Link, useNavigate} from "react-router-dom";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import {Alert, CircularProgress} from "@mui/material";
 
 const Register = () => {
     const [err, setErr] = useState(false);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate()
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
+    const [avatar,setAvatar] = useState("")
+    const [changeAvatar, setChangeAvater] = useState(false);
+    const [fileValue,setFileValue] = useState(null)
+    const handleFileChange = (e)=>{
+        const file = e.target.files[0];
+
+        if (file){
+            const reader = new FileReader()
+            setFileValue(file)
+            reader.onloadend = () => {
+                setAvatar(reader.result)
+            };
+            reader.readAsDataURL(file)
+            setChangeAvater(true);
+        } else {
+            setChangeAvater(false)
+            setFileValue("")
+        }
+    }
+    const handleSubmit = async (values) => {
+
+        const displayName = values.displayName
+        const email = values.email
+        const password = values.password
+        const file = fileValue
+
+        console.log(file)
 
         try {
             //Create user
@@ -45,10 +67,10 @@ const Register = () => {
                         //create empty user chats on firestore
                         await setDoc(doc(db, "userChats", res.user.uid), {});
                         navigate("/")
+                        setErr(false)
                     } catch (err) {
                         console.log(err);
                         setErr(true);
-                        setLoading(false);
                     }
                 });
             });
@@ -59,29 +81,64 @@ const Register = () => {
     };
 
     return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">Lama Chat</span>
-                <span className="title">Register</span>
-                <form onSubmit={handleSubmit}>
-                    <input required type="text" placeholder="display name" />
-                    <input required type="email" placeholder="email" />
-                    <input required type="password" placeholder="password" />
-                    <input required style={{ display: "none" }} type="file" id="file" />
-                    <label htmlFor="file">
-                        <img src={Add} alt="" />
-                        <span>Add an avatar</span>
-                    </label>
-                    <button disabled={loading}>Sign up</button>
-                    {loading && "Uploading and compressing the image please wait..."}
-                    {err && <span>Something went wrong</span>}
-                </form>
-                <p>
-                    You do have an account?<Link to="/login">Login</Link>
-                </p>
+            <div className="formContainer">
+                <div className="formWrapper">
+                    <span className="logo">Omit Chat</span>
+                    <span className="title">Register</span>
+                    <Formik
+                        initialValues={{
+                            displayName: "",
+                            email: "",
+                            password: "",
+                            file: ""
+                        }}
+                        validate={values => {
+                            const error = {}
+                            if (!values.displayName) {
+                                error.displayName = "username is required";
+                            }
+                            if (!values.email) {
+                                error.email = 'email is required';
+                            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                                error.email = 'email is invalid';
+                            }
+                            if (!values.password) {
+                                error.password = 'password is required';
+                            } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/i.test(values.password)) {
+                                error.password =
+                                    'password must have one letter and one number and have at least 6 characters';
+                            }
+
+                            return error;
+                        }}
+                        onSubmit={(values) => handleSubmit(values)}
+                    >
+                        {({isSubmitting}) => (
+                            <Form>
+                                <Field type="text" placeholder="username" name="displayName"/>
+                                <ErrorMessage className="errorMessage" name="displayName" component="div"/>
+                                <Field type="email" placeholder="email" name="email"/>
+                                <ErrorMessage className="errorMessage" name="email" component="div"/>
+                                <Field type="password" placeholder="password" name="password"/>
+                                <ErrorMessage className="errorMessage" name="password" component="div"/>
+                                <Field style={{display: "none"}} type="file" placeholder="file" name="file" id="file" onChange={handleFileChange}/>
+                                <label htmlFor="file">
+                                    <img style={{width: "40px",height: "40px",borderRadius: "50%"}} src={changeAvatar ? avatar : Add} alt=""/>
+                                    <span>Add an avatar</span>
+                                </label>
+                                <button type="submit" disabled={isSubmitting}>
+                                    { isSubmitting ? <CircularProgress /> : "Sign up"}
+                                </button>
+                                { err && <Alert severity="error">Error communicating with the server</Alert>}
+                            </Form>
+                        )}
+                    </Formik>
+                    <p>
+                        You do have an account?<Link to="/login">Login</Link>
+                    </p>
+                </div>
             </div>
-        </div>
-    );
-};
+            );
+            };
 
 export default Register;
