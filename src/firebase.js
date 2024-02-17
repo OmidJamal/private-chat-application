801ff2425879b38a2c8370473e6cfb0fd68bc786
OmidJamal <omidjamal.oji@gmail.com>
2024-02-17 23:52:22 +0330

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,6 @@ import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore"
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDvD4QbI360jChodQAz2-znJtd8jykASZM",
-//     authDomain: "private-chat-1180b.firebaseapp.com",
-//     projectId: "private-chat-1180b",
-//     storageBucket: "private-chat-1180b.appspot.com",
-//     messagingSenderId: "649999119296",
-//     appId: "1:649999119296:web:2703d1ed03861eeebece25"
-// };
 const firebaseConfig = {
 
     apiKey: "AIzaSyAcqKEQJs_e_bBgfcfGbUzdy4etRaC3A48",
@@ -28,6 +20,9 @@ const firebaseConfig = {
     appId: "1:320930258142:web:e92b4004c8cbbe8ef8364f"
 
 };
+
+
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
